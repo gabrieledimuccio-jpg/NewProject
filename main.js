85f78cefd19297b0eb55ac24385bdf6d69f34516
Gabriele Di Muccio <gabrieledimuccio@gmail.com>
2026-01-12 16:08:53 +0100

    svolto selfwork variabili

---
+++ main.js
@@ -0,0 +1,17 @@
+let num1 = 1;
+console.log(num1);
+
+let num2 = 10;
+console.log(num2);
+
+let stringa = "Ciao a tutti, sono una stringa";
+console.log(stringa);
+ 
+stringa = "Ciao adesso sono stringa numero 2";
+console.log(stringa);
+
+const PIGRECO = 3.14159265359;
+console.log(PIGRECO);
+
+PIGRECO = 3.16;
+console.log(PIGRECO);
